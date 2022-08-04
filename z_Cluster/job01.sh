7116d67079945234ce8cc5286c75b8d3023b2266
caramirezs <kamandramsan@gmail.com>
2022-08-04 16:40:32 -0500

    Versión 2.3 (ago 4) Summary!

--- z_Cluster/job01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J job01.sh
-#SBATCH --partition=defq
-#SBATCH --get-user-env
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=20
-#SBATCH --gres=gpu:4
-
-srun ./ML_for_DD/python 01_XGBoost_BayesSearchCV.py
